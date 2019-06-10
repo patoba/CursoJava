23145d73d2e5bed2781268f3ed2af0a722b0335e
patoba <patobarrero@gmail.com>
2019-06-09 22:57:57 -0500

    final

---
+++ Java Basico/Encapsulamiento/Mundo/olimpo/Zeus.java
@@ -0,0 +1,12 @@
+package olimpo;
+
+import tierra.Persona;
+
+public class Zeus{
+
+    public static void main(String[] args) {
+        Persona p = new Persona("Paco");
+        
+        System.out.println("Cree a " + p);
+    }
+}
---
+++ Java Basico/Encapsulamiento/Mundo/tierra/Persona.java
@@ -0,0 +1,14 @@
+package tierra;
+
+public class Persona{
+    private String nombre;
+
+    public Persona(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String toString(){
+        return this.nombre;
+    }
+    // Modificar modificadores de persona y usar getters y setters.
+}
